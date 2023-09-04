936b07f5424bcf468f37d23b0aadd846f67c0596
jimeneztadeocruz <jimeneztadeocruz@gmail.com>
2023-09-04 00:17:35 -0500

    modificando la data

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! ORIBANDO a';
+    return 'Hello World! ORIBANDO ACTIONS2';
   }
 }
