89944eb8a229547bf28365b2b2d6728c9faacb04
cruztadeoxid <128651375+cruztadeoxid@users.noreply.github.com>
2023-09-05 22:35:49 -0500

    add microservices

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './app/auth/auth.module';
 import { EnvironmentConfigModule } from './database/infrastructure/environment/environment-config.module';
+import { ChatGateway } from './app/chat/chat.gateway';
 
 @Module({
 	imports: [
 		AuthModule,
 		EnvironmentConfigModule
 	],
+	providers: [ChatGateway],
 })
 export class AppModule {}
---
+++ src/app/chat/chat.gateway.ts
@@ -0,0 +1,25 @@
+import { WebSocketServer, WebSocketGateway, SubscribeMessage, MessageBody, ConnectedSocket, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
+import {Socket, Server} from 'socket.io'
+
+@WebSocketGateway(3001, {
+  cors: '*'
+})
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server
+
+  handleConnection(client: any) {
+    // Handle connection event
+  }
+
+  handleDisconnect(client: any) {
+    // Handle disconnection event
+  }
+
+  @SubscribeMessage('message')
+  handleMessage(@MessageBody() data: string, @ConnectedSocket() client: Socket) {
+    // Handle received message
+    console.log(data, 'data')
+    this.server.emit('message', data); // Broadcast the message to all connected clients
+  }
+}
