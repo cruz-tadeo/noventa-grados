a65527bdfd20732c8b1e85470a745e4aea501416
jimeneztadeocruz <jimeneztadeocruz@gmail.com>
2023-09-06 00:34:40 -0500

    conexion

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './app/auth/auth.module';
 import { EnvironmentConfigModule } from './database/infrastructure/environment/environment-config.module';
 import { ChatGateway } from './app/chat/chat.gateway';
+import { ConnectionDatabaseModule } from './database/database.module';
 
 @Module({
 	imports: [
 		AuthModule,
+		ConnectionDatabaseModule,
 		EnvironmentConfigModule
 	],
 	providers: [ChatGateway],
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EnvironmentConfigModule } from './infrastructure/environment/environment-config.module';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
 	imports: [
+		MongooseModule.forRoot('mongodb://Hostinger:$tr0ngPa$$w0rD@86.38.203.219:27017/noventa-grados?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.6'),
 		EnvironmentConfigModule
 	]
 })
